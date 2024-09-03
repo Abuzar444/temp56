be54a9be7555080ae997ef3c9ca6d2c408d74754
= <=>
2024-09-03 15:12:35 +0500

    commit

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const bucket = "home-away";
+const bucket = "home-away-2";
 
 const url = process.env.SUPABASE_URL as string;
 const key = process.env.SUPABASE_KEY as string;
