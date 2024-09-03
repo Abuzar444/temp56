241c71161a020b7f31e3e7c8cec306202567ed57
= <=>
2024-09-03 16:41:37 +0500

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "legrbgxzorcxpooaukvr.supabase.co",
+        hostname: "lqwsarshfucesldaurcz.supabase.co",
       },
     ],
   },
